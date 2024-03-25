8836f59c3405911e41ee6d7462ef3c32085b9fba
wajdi khelifi <133996419+wajdi-khelifi@users.noreply.github.com>
2024-03-25 20:17:13 +0100

    Delete apps directory

--- apps/courses/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Course(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    # Add other fields like instructor, duration, etc.
-
-    def __str__(self):
-        return self.title
